9f2291c01f71731ac284026c423643864323d85d
alexanderota119 <alexanderota119@gmail.com>
2023-03-22 21:58:53 +0700

    Fix badges

--- src/components/badge.js
+++ src/components/badge.js
@@ -7,14 +7,14 @@ const Badges = () => {
       <a href="https://github.com/rfauzi44/vehicle-rental-api">
         <img
           className="badge"
-          src="https://img.shields.io/static/v1?label=rfauzi44&message=vehicle-rental-api&color=blue&logo=go&style=for-the-badge"
+          src="https://img.shields.io/static/v1?label=rfauzi44&message=vehicle-rental-api&color=blue&logo=go"
           alt="Go repository badge"
         />
       </a>
       <a href="https://github.com/rfauzi44/vehicle-rental-web">
         <img
           className="badge"
-          src="https://img.shields.io/static/v1?label=rfauzi44&message=vehicle-rental-web&color=blue&logo=react&style=for-the-badge"
+          src="https://img.shields.io/static/v1?label=rfauzi44&message=vehicle-rental-web&color=blue&logo=react"
           alt="React repository badge"
         />
       </a>
